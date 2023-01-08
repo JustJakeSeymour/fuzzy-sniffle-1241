def4b1389da5b3000bed25d9f2381f1072067bf5
J Seymour <JustJakeSeymour@gmail.com>
2023-01-08 11:36:25 -0700

    build uniq ingredients method

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,8 +1,10 @@
 class Chef < ApplicationRecord
   validates_presence_of :name
   has_many :dishes
+  has_many :dish_ingredients, through: :dishes
+  has_many :ingredients, through: :dish_ingredients
 
   def unique_ingredients
-    
+    self.ingredients.uniq
   end
 end
--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe Chef, type: :model do
   end
   describe "relationships" do
     it {should have_many :dishes}
+    it {should have_many :dish_ingredients}
+    it {should have_many :ingredients}
   end
 
   describe "instance methods" do
