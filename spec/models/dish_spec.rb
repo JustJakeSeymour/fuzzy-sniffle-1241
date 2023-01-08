371e5b52f4fed27a6737ac820b067efec131101f
J Seymour <JustJakeSeymour@gmail.com>
2023-01-08 11:44:05 -0700

    100 pct coverage

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -9,4 +9,17 @@ RSpec.describe Dish, type: :model do
     it {should belong_to :chef}
     it {should have_many :dish_ingredients}
   end
+
+  describe "instance method" do
+    let!(:chef1) {create(:chef)}
+    let!(:dish1) {create(:dish, chef_id: chef1.id)}
+    let!(:ingredient1) {create(:ingredient, calorie: 200)}
+    let!(:ingredient2) {create(:ingredient, calorie: 350)}
+    let!(:dish_ingredient1) {create(:dish_ingredient, dish_id: dish1.id, ingredient_id: ingredient1.id)}
+    let!(:dish_ingredient2) {create(:dish_ingredient, dish_id: dish1.id, ingredient_id: ingredient2.id)}
+
+    it "total_calorie" do
+      expect(dish1.total_calorie).to eq(550)  
+    end
+  end
 end
